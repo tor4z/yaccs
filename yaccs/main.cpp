689e2b6525f5c195a698142d41719a8343f5c80f
tor4z <vwenjie@hotmail.com>
2025-07-10 23:06:41 +0800

    Explore onnx model

--- yaccs/main.cpp
+++ yaccs/main.cpp
@@ -13,8 +13,37 @@ int main(int argc, char** argv)
     ifs.close();
 
     std::cout << "name: " << model.graph().name() << "\n";
-    std::cout << "doc: " << model.graph().doc_string() << "\n";
+    std::cout << "=======\n";
+
+    for (const auto& node: model.graph().node()) {
+        std::cout << node.name() << "\n";
+        std::cout << "OP Type: " << node.op_type() << "\n";
+        std::cout << "Attribute name: ";
+        for (const auto& attr: node.attribute()) {
+            std::cout << attr.name() << ", ";
+        }
+        std::cout << "\n";
+        std::cout << "Attribute type: ";
+        for (const auto& attr: node.attribute()) {
+            std::cout << attr.type() << "-" << attr.f() << "-" << attr.i() << "-hasG-" << attr.has_g() << ", ";
+        }
+        std::cout << "\n";
+
+        std::cout << "Input: ";
+        for (const auto& input : node.input()) {
+            std::cout << input << ", ";
+        }
+
+        std::cout << "\nOutput: ";
+        for (const auto& output : node.output()) {
+            std::cout << output << ", ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout << "=======\n";
     for (const auto& it : model.graph().initializer()) {
+        std::cout << "name: " << it.name() << "\n";
         std::cout << "dt: " << it.data_type() << "\n";
         int num_elems{1};
         std::cout << "dims: [";
@@ -23,13 +52,13 @@ int main(int argc, char** argv)
             num_elems *= d;
         }
         std::cout << "]\n";
-        for (int i = 0; i < num_elems; ++i) {
-            float v;
-            auto raw{le32toh(*reinterpret_cast<const uint32_t*>(it.raw_data().data() + i * 4))};
-            v =  *reinterpret_cast<float*>(&raw);
-            std:: cout << v << " ";
-        }
-        std::cout << "\n";
+        // for (int i = 0; i < num_elems; ++i) {
+        //     float v;
+        //     auto raw{le32toh(*reinterpret_cast<const uint32_t*>(it.raw_data().data() + i * 4))};
+        //     v =  *reinterpret_cast<float*>(&raw);
+        //     std:: cout << v << " ";
+        // }
+        // std::cout << "\n";
     }
 
     std::cout << "input size: " << model.graph().input().size() << "\n";

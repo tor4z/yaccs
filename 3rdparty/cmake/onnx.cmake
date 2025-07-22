8dab188e9e29f3e495d5117bf08699ff0e54ed8f
tor4z <vwenjie@hotmail.com>
2025-07-22 21:37:59 +0800

    Update onnx module with latest protobuf

--- 3rdparty/cmake/onnx.cmake
+++ 3rdparty/cmake/onnx.cmake
@@ -1,7 +1,11 @@
 find_package(Protobuf REQUIRED)
+find_package(absl REQUIRED)
 
 include_directories(${PROJECT_SOURCE_DIR}/3rdparty/onnx)
 
 set(ONNX_SRC ${PROJECT_SOURCE_DIR}/3rdparty/onnx/onnx.pb.cc)
 
-set(ONNX_DPE_LIBS ${Protobuf_LIBRARIES})
+set(ONNX_DPE_LIBS
+    ${PROTOBUF_LIBRARY}
+    absl::absl_check
+)

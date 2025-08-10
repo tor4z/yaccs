8cf01b350ea90fe16b4b6c2da376915af98a7e56
tor4z <vwenjie@hotmail.com>
2025-08-10 21:44:31 +0800

    Fix example bugs

--- examples/01-basic_mlp/main.cpp
+++ examples/01-basic_mlp/main.cpp
@@ -17,27 +17,36 @@ int main()
         auto instance{cov::App::new_instance()};
         instance.load_shader(shader_path);
 
-        std::vector<float> input{1.0, 2.0, 3.0, 4.0};
-        std::vector<float> output(4);
+        std::vector<char> input(4 + 6 * 4 + 4 * 4);
+        uint32_t dims{2};
+        uint32_t shape[6]{1, 4, 0, 0, 0, 0};
+        memcpy(input.data(), &dims, sizeof(dims));
+        memcpy(input.data() + 4, shape, 6 * sizeof(uint32_t));
+        std::vector<char> output(4 + 6 * 4 + 4 * 4, 0);
 
         instance.set_inputs({
-            {input.data(), input.size() * sizeof(input.at(0))},
+            {input.data(), input.size()},
         });
         instance.def_output(output.size() * sizeof(output.at(0)));
 
         if (!instance.execute({32, 32, 1})) {
             std::cerr << "Execute shader program failed\n";
         }
-        // instance.get_output(output.data(), output.size() * sizeof(output.at(0)));
+        instance.get_output(output.data(), output.size());
         // The instance will be automatically destroy here.
-        
-        std::cout << "Output: ";
-        for (auto it: output) {
-            std::cout << it << " ";
+
+        std::cout << "Output:\n";
+        std::cout << "dims: " << *reinterpret_cast<uint32_t*>(output.data()) << "\n";
+        std::cout << "shape: ";
+        for (int i = 0; i < 6; ++i) {
+            std::cout << *reinterpret_cast<uint32_t*>(output.data() + 4 * (i + 1)) << " ";
+        }
+        std::cout << "\ndata: ";
+        for (int i = 0; i < 4; ++i) {
+            std::cout << *reinterpret_cast<float*>(output.data() + 4 * (i + 1 + 6)) << " ";
         }
         std::cout << "\n";
     }
-
 
     return 0;
 }
